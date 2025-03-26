de72d1e4b56212786153b4d0f53125abd268c0ff
Gonçalo Peixoto <goncalopeixoto03@gmail.com>
2025-03-26 10:16:00 +0000

    splash start

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,11 @@ const routes: Routes = [
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   },
+  {
+    path: 'splash',
+    loadChildren: () => import('./splash/splash.module').then( m => m.SplashPageModule)
+  },
+
 
 
 ];
---
+++ src/app/splash/splash.module.ts
@@ -0,0 +1,21 @@
+import { IonicModule } from '@ionic/angular';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { SplashPage } from './splash.page';
+
+
+import { SplashPageRoutingModule } from './splash-routing.module';
+import { ComponentsModule } from 'src/app/components/components.module';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+@NgModule({
+  imports: [
+    IonicModule,
+    CommonModule,
+    FormsModule,
+    ComponentsModule,
+    SplashPageRoutingModule
+  ],
+  declarations: [SplashPage]
+})
+export class SplashPageModule {}
